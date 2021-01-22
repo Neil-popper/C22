f9c76370e038eca233c879eb11326556d4ac9fc1
Neil-popper <77514497+Neil-popper@users.noreply.github.com>
2021-01-22 16:46:26 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var object;
+var ball;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var object_options ={
+        isStatic: true
+    }
+    var ball_options ={
+        restitution: 0.5
+    }
+
+   object= Bodies.rectangle(200,390,400,20,object_options);
+    World.add(world,object);
+    
+   ball= Bodies.circle(100,100,10,ball_options);
+   World.add(world,ball);
+
+    console.log(object);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(object.position.x,object.position.y,400,50);
+    ellipseMode(RADIUS)
+    ellipse(ball.position.x,ball.position.y,10,10)
+}
